843d834bd07985a80c03eb3242249062f8e61b9f
Mathieu <mathieux51@gmail.com>
2019-12-16 08:20:43 +0100

    Rename folders

---
+++ chatpubsub/js/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+function App() {
+  return <Title>Title</Title>
+}
+
+export default App
